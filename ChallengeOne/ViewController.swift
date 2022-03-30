f02d33c8c0850852135dac8dba4691404e22804e
Fernando Luiz Goulart <fernando.goulart_cit@prestador.globo>
2022-03-30 11:27:25 -0300

    add swiftlint

--- ChallengeOne/ViewController.swift
+++ ChallengeOne/ViewController.swift
@@ -1,19 +1,8 @@
-//
-//  ViewController.swift
-//  ChallengeOne
-//
-//  Created by Fernando Luiz Goulart on 30/03/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+public final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
